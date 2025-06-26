0d4176d9747963de1f14b9a0cb362a6abf9554ed
ButabGitHub <94287561+ButabGitHub@users.noreply.github.com>
2025-06-26 17:39:42 +0300

    Node++

--- IceLoftSDK/Node.cpp
+++ IceLoftSDK/Node.cpp
@@ -1,1 +1,19 @@
 #include "Node.h"
+
+void Node::Init() {
+	for (const std::unique_ptr<Node>& child : children) {
+		child->Init();
+	}
+}
+
+std::unique_ptr<Node>& Node::GetChild(const int child) {
+	return children.at(child);
+}
+
+//std::unique_ptr<Node>& Node::GetChild(const std::string child) {
+//	for (const std::unique_ptr<Node>& _child : children) {
+//		if (_child.name == child) {
+//			return children.at(child);
+//		}
+//	}
+//}
--- IceLoftSDK/Node.h
+++ IceLoftSDK/Node.h
@@ -9,6 +9,7 @@ class Node {
 private:
     Node* parent;
     std::vector<std::unique_ptr<Node>> children; // Should I use vector or array for this??
+                                                 // Answer: I can't use array for this
 
 public:
     std::string name;
@@ -16,14 +17,16 @@ public:
     // Lifecycle stuff
     virtual void Init();
     virtual void Update(double delta);
-    virtual void Destroy(); // End is such an awful names for this function (probably lol) but I'll keep it like that for now
+    virtual void End(); // End is such an awful names for this function (probably lol) but I'll keep it like that for now
 
     // Node interaction stuff
     void AddChild(std::unique_ptr<Node>);
 
-    std::unique_ptr<Node> GetChild(int child); // GetChild by it's ID
-    std::unique_ptr<Node> GetChild(std::string child); // GetChild by it's name
+    std::unique_ptr<Node>& GetChild(const int child); // GetChild by it's ID
+    std::unique_ptr<Node>& GetChild(const std::string child); // GetChild by it's name
 
+    void Destroy(); // Destroy the Node
+    
     std::vector<std::unique_ptr<Node>> GetChildren();
 
     // Idk what I can add here
