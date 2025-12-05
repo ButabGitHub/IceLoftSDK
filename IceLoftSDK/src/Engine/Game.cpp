fd3d111a9f00b141c45b179b3d23d7ddb816ffd3
ButabGitHub <94287561+ButabGitHub@users.noreply.github.com>
2025-12-05 18:04:41 +0100

    lf2

--- IceLoftSDK/src/Engine/Game.cpp
+++ IceLoftSDK/src/Engine/Game.cpp
@@ -18,13 +18,14 @@ void Game::Init(GameConfig conf) {
     SetConsoleMode(hInput, ENABLE_VIRTUAL_TERMINAL_INPUT);
     HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleMode(hOutput, ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+#endif // _WIN32
 
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_SAMPLES, 4);
-#endif // _WIN32
+
 
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Only for MacOS
--- IceLoftSDK/src/Engine/Log.h
+++ IceLoftSDK/src/Engine/Log.h
@@ -3,5 +3,5 @@
 #include <iostream>
 
 #define LOG(x) std::cout << x << "\n";
-#define WARN(x) std::cerr << "\x1b[33m" << x << "\033[0m\n";
+#define WARN(x) std::cout << "\x1b[33m" << x << "\033[0m\n";
 #define ERROR(x) std::cerr << "\x1b[31m" << x << "\033[0m\n";
