efc8832055b66f637f805cab97128d4adbe3f599
ButabGitHub <94287561+ButabGitHub@users.noreply.github.com>
2026-01-16 16:09:37 +0100

    Better now.

--- src/Engine/Log.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-#define LOG(x) std::cout << x << "\n";
-#define WARN(x) std::cout << "\x1b[33m" << x << "\033[0m\n";
-#define ERR(x) std::cerr << "\x1b[31m" << x << "\033[0m\n";
--- src/Level/Root.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <Base/Node.h>
-
-extern std::unique_ptr<Node> root;
-
